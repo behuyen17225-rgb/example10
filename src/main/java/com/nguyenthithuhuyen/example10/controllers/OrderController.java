5b3e0a906e095977330e54e3cac4a93a4df09f91
cuong0374522557-beep <cuong.0374522557@gmail.com>
2026-01-14 10:09:01 +0700

    cap nhap

--- src/main/java/com/nguyenthithuhuyen/example10/controllers/OrderController.java
+++ src/main/java/com/nguyenthithuhuyen/example10/controllers/OrderController.java
@@ -33,7 +33,6 @@ public class OrderController {
     /* =====================================================
        KHÁCH TẠO / THÊM ORDER (CÓ SOCKET)
        ===================================================== */
-@PreAuthorize("hasAnyRole('USER','MODERATOR')")
 @PostMapping
 public ResponseEntity<?> createOrder(@RequestBody Order order) {
     String username = SecurityContextHolder
