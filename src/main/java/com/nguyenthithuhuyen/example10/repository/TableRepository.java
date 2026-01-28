509ce43a7afc064b95475449468b7f94303f98d6
cuong0374522557-beep <cuong.0374522557@gmail.com>
2026-01-28 20:27:31 +0700

    Order

--- src/main/java/com/nguyenthithuhuyen/example10/repository/TableRepository.java
+++ src/main/java/com/nguyenthithuhuyen/example10/repository/TableRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface TableRepository extends JpaRepository<TableEntity, Long> {
     List<TableEntity> findByStatus(Status status);
-    TableEntity findByNumber(Integer number);
+    TableEntity findByNumber(Integer tableNumber);
 }
