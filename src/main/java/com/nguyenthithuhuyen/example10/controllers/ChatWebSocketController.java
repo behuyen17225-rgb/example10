a42da815aeb463763140da63d8be5b62a4fa1124
cuong0374522557-beep <cuong.0374522557@gmail.com>
2026-01-31 11:03:49 +0700

    up code n

--- src/main/java/com/nguyenthithuhuyen/example10/controllers/ChatWebSocketController.java
+++ src/main/java/com/nguyenthithuhuyen/example10/controllers/ChatWebSocketController.java
@@ -2,14 +2,17 @@ package com.nguyenthithuhuyen.example10.controllers;
 
 import com.nguyenthithuhuyen.example10.payload.websocket.WsChatMessage;
 import com.nguyenthithuhuyen.example10.security.services.SecurityChatService;
+import com.nguyenthithuhuyen.example10.security.services.UserDetailsImpl;
 import com.nguyenthithuhuyen.example10.entity.Conversation;
 import com.nguyenthithuhuyen.example10.repository.ConversationRepository;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.nio.file.attribute.UserPrincipal;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -36,9 +39,21 @@ public class ChatWebSocketController {
         Conversation c = conversationRepo
                 .findById(message.getConversationId())
                 .orElseThrow();
+if ("STAFF".equals(message.getSender()) && c.getStaffId() == null) {
 
-        c.setUpdatedAt(LocalDateTime.now());
-        conversationRepo.save(c);
+    Authentication auth = SecurityContextHolder
+            .getContext()
+            .getAuthentication();
+
+    if (auth != null && auth.isAuthenticated()) {
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof UserDetailsImpl userDetails) {
+            Long staffId = userDetails.getId(); // ✅ ĐÚNG CLASS
+            c.setStaffId(staffId);
+        }
+    }
+}
 
         /* ================== 3️⃣ SEND CHAT ================== */
         messagingTemplate.convertAndSend(
